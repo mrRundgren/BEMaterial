ab27cfd5c67d9bc351c3ea40af57538a7855409f
anders.rundgren <anders.rundgren@elicit.se>
2022-06-20 14:35:53 +0200

    added fake now playing logic

---
+++ Pages/_Examples/Spotify/Demo/Models/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace BEMaterial.Pages._Examples.Spotify.Demo.Models
+{
+    public enum ImageType { Album, Person };
+
+    public class Image
+    {
+        public ImageType Type { get; set; } = ImageType.Album;
+        public string ImageUrl { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+    }
+}
