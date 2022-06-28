f6a873ef969e1da7533a497ac6aa9fc4d64e391d
anders.rundgren <anders.rundgren@elicit.se>
2022-06-28 15:20:57 +0200

    Trying to fix intersect observer

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using BEMaterial;
 using BlazorPro.BlazorSize;
+using Ljbc1994.Blazor.IntersectionObserver;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMediaQueryService();
+builder.Services.AddIntersectionObserver();
 
 await builder.Build().RunAsync();
