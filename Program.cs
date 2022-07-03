d52bd77311f2d7c30304bde94ed7397b386893d6
anders.rundgren <anders.rundgren@elicit.se>
2022-07-03 19:28:14 +0200

    Added media to query to deal with menu to be open on desktop

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMediaQueryService();
+builder.Services.AddScoped<IResizeListener, ResizeListener>();
 builder.Services.AddIntersectionObserver();
 
 await builder.Build().RunAsync();
